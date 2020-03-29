206eee41172acec4795cd94e0e19c946c0edf345
wisetechi1911 <62833423+wisetechi1911@users.noreply.github.com>
2020-03-29 20:52:27 +0530

    created new ApprovalCard component and wrapped previous component CommentDetail with ApprovalCard and passed the children props thorugh it to display the comment details in a card

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import faker from 'faker';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail'
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return <div className="ui container comments">
-        <CommentDetail author="Ravi" time="6:00PM" post="hello this is Ravi" avatar={faker.image.avatar()}/>
-        <CommentDetail author="Kumar" time="6:08PM" post="hello this is Kumar" avatar={faker.image.avatar()}/>
-        <CommentDetail author="Vykuntham" time="6:17PM" post="hello this is Vykuntham" avatar={faker.image.avatar()}/>
+        <ApprovalCard > <CommentDetail author="Ravi" time="6:00PM" post="hello this is Ravi" avatar={faker.image.avatar()} />  </ApprovalCard>
+        <ApprovalCard > <CommentDetail author="Kumar" time="6:08PM" post="hello this is Kumar" avatar={faker.image.avatar()} /> </ApprovalCard>
+        <ApprovalCard > <CommentDetail author="Vykuntham" time="6:17PM" post="hello this is Vykuntham" avatar={faker.image.avatar()} /> </ApprovalCard>
     </div>
 }
 
