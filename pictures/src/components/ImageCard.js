90331df25d32dee0cb54f01bb5d47cd33744a660
wisetechi1911 <62833423+wisetechi1911@users.noreply.github.com>
2020-04-02 21:28:36 +0530

    minor changes

--- pictures/src/components/ImageCard.js
+++ pictures/src/components/ImageCard.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
-    constructor(props){
-        super(props);
-
-        this.imageRef = React.createRef();
-    }
-
-    componentDidMount(){
-        console.log(this.imageRef.current.clientHeight);
-    }
+    
     render() { 
         const {description,urls} = this.props.image;
         return ( 
-            <img ref={this.imageRef}
-                 alt={description} 
+            <img alt={description} 
                  src={urls.regular}/>
          );
     }
